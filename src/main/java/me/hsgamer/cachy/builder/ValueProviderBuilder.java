e64a1e6dacdffffcabd1732181e49b5d7b06d799
hsgamer <huynhqtienvtag@gmail.com>
2025-05-16 10:29:58 +0700

    update usage

--- src/main/java/me/hsgamer/cachy/builder/ValueProviderBuilder.java
+++ src/main/java/me/hsgamer/cachy/builder/ValueProviderBuilder.java
@@ -1,15 +1,21 @@
 package me.hsgamer.cachy.builder;
 
 import me.hsgamer.hscore.builder.FunctionalMassBuilder;
+import me.hsgamer.hscore.common.CollectionUtils;
 import me.hsgamer.topper.spigot.value.statistic.StatisticValueProvider;
 import me.hsgamer.topper.value.core.ValueProvider;
 import org.bukkit.entity.Player;
 
-import java.util.Map;
+import java.util.*;
 
 public class ValueProviderBuilder extends FunctionalMassBuilder<ValueProviderBuilder.Input, ValueProvider<Player, String>> {
     public ValueProviderBuilder() {
-        register(input -> StatisticValueProvider.fromMap(input.data).thenApply(Object::toString), "statistic");
+        register(input -> {
+            String statistic = Optional.ofNullable(input.data.get("statistic")).map(Objects::toString).orElse(null);
+            List<String> materials = Optional.ofNullable(input.data.get("material")).map(CollectionUtils::createStringListFromObject).orElse(Collections.emptyList());
+            List<String> entityTypes = Optional.ofNullable(input.data.get("entity-type")).map(CollectionUtils::createStringListFromObject).orElse(Collections.emptyList());
+            return StatisticValueProvider.fromRaw(statistic, materials, entityTypes).thenApply(Object::toString);
+        }, "statistic");
     }
 
     @Override
